11903c989b857272c2801a27a92484a9a19adb6c
kitsuyui <kitsuyui@kitsuyui.com>
2024-12-28 03:03:56 +0900

    Add softmax test cases
    - the sum of the softmax output should be 1
    - the softmax output should be between 0 and 1

--- packages/mymath/src/softmax/index.spec.ts
+++ packages/mymath/src/softmax/index.spec.ts
@@ -1,15 +1,22 @@
 import { describe, expect, it, jest } from '@jest/globals'
 
+import { sum } from '../array'
 import { softmax, softmaxWithTemperature } from './index'
 
 describe('softmaxWithTemperature', () => {
   it('should return the correct result', () => {
     const weights = [1, 2, 3, 4, 5]
     const temperature = 1
+    const result = softmaxWithTemperature(weights, temperature)
     expect(softmaxWithTemperature(weights, temperature)).toEqual([
       0.011656230956039605, 0.03168492079612427, 0.0861285444362687,
       0.23412165725273662, 0.6364086465588308,
     ])
+    expect(sum(result)).toBeCloseTo(1)
+    for (const probability of result) {
+      expect(probability).toBeGreaterThanOrEqual(0)
+      expect(probability).toBeLessThanOrEqual(1)
+    }
   })
 
   it('should return an empty array when the input is an empty array', () => {
@@ -22,10 +29,16 @@ describe('softmaxWithTemperature', () => {
 describe('softmax', () => {
   it('should return the correct result', () => {
     const weights = [1, 2, 3, 4, 5]
-    expect(softmax(weights)).toEqual([
+    const result = softmax(weights)
+    expect(result).toEqual([
       0.011656230956039605, 0.03168492079612427, 0.0861285444362687,
       0.23412165725273662, 0.6364086465588308,
     ])
+    expect(sum(result)).toBeCloseTo(1)
+    for (const probability of result) {
+      expect(probability).toBeGreaterThanOrEqual(0)
+      expect(probability).toBeLessThanOrEqual(1)
+    }
   })
 
   it('should return an empty array when the input is an empty array', () => {
