e58c783e8ba8b27999350e56edf52f3e0c260608
kitsuyui <kitsuyui@kitsuyui.com>
2025-06-05 17:37:14 +0900

    Implement intervalMapper and clamp functions in mymath package
    - intervalMapper: functions to map values between intervals such as [0, 1], [0, +∞), etc.
    - Add more clamp functions to handle infinity and NaN cases.

--- packages/mymath/src/clamp.ts
+++ packages/mymath/src/clamp.ts
@@ -21,3 +21,38 @@ export const clamp = (value: number, range: [number, number]): number => {
   const [min, max] = range
   return Math.min(Math.max(value, min), max)
 }
+
+/**
+ * Clamps a value between the minimum and maximum safe integer values
+ * @param value
+ * @returns The clamped value between Number.MIN_SAFE_INTEGER and Number.MAX_SAFE_INTEGER
+ */
+export const clampMinAndMax = (value: number): number => {
+  return clamp(value, [Number.MIN_VALUE, Number.MAX_VALUE])
+}
+
+/**
+ * Clamps a value between the minimum and maximum safe integer values
+ * @param value
+ * @returns The clamped value between Number.MIN_SAFE_INTEGER and Number.MAX_SAFE_INTEGER
+ */
+export const clampMinAndMaxInt = (value: number): number => {
+  return clamp(value, [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER])
+}
+
+type NumericFunction = (value: number) => number
+
+/**
+ * Wraps a function to return NaN if it throws an error
+ * @param fn The function to wrap
+ * @returns A new function that returns NaN if the original function throws an error
+ */
+export const errorToNaN = (fn: NumericFunction): NumericFunction => {
+  return (value: number): number => {
+    try {
+      return fn(value)
+    } catch (error) {
+      return Number.NaN
+    }
+  }
+}
