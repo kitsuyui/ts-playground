977d564c2c8cffc75e8cfea4ebcd03c1772a92f7
kitsuyui <kitsuyui@kitsuyui.com>
2025-06-15 01:07:26 +0900

    Validate package build assets and exports in package.json
    - Check all packages are properly built and exported.

---
+++ packages/mymath/src/index.ts
@@ -0,0 +1,18 @@
+export { sigmoid } from './sigmoid'
+export { logOfBase, logFnOfBase } from './log'
+export {
+  sum,
+  product,
+  average,
+  arithmeticMean,
+  cubicMean,
+  generalizedMean,
+  geometricMean,
+  harmonicMean,
+  rootMeanSquare,
+} from './array'
+export { clamp } from './clamp'
+export * from './clamp'
+export { softmax, softmaxWithTemperature } from './softmax'
+export * as polar from './polar'
+export * as intervalMapper from './intervalMapper'
