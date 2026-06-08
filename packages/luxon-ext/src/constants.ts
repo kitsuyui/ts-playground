89b6e23d85d933b3ec1e917bb9d1a396c8ff1c24
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-08 15:51:54 +0900

    refactor: make luxon thresholds units explicit

--- packages/luxon-ext/src/constants.ts
+++ packages/luxon-ext/src/constants.ts
@@ -1,19 +1,19 @@
-const MILLIS = 1
-const SECONDS = MILLIS * 1000
-const MINUTES = SECONDS * 60
-const HOURS = MINUTES * 60
-const DAYS = HOURS * 24
-const WEEKS = DAYS * 7
-const YEARS = DAYS * 365.25
-const MONTHS = YEARS / 12
+const MILLISECOND_MS = 1
+const SECOND_MS = MILLISECOND_MS * 1000
+const MINUTE_MS = SECOND_MS * 60
+const HOUR_MS = MINUTE_MS * 60
+const DAY_MS = HOUR_MS * 24
+const WEEK_MS = DAY_MS * 7
+const YEAR_MS = DAY_MS * 365.25
+const MONTH_MS = YEAR_MS / 12
 
-export const HALF_OF_TIME_UNITS = {
-  years: 0.5 * YEARS,
-  months: 0.5 * MONTHS,
-  weeks: 0.5 * WEEKS,
-  days: 0.5 * DAYS,
-  hours: 0.5 * HOURS,
-  minutes: 0.5 * MINUTES,
-  seconds: 0.5 * SECONDS,
-  milliseconds: 0.5 * MILLIS,
+export const HALF_OF_TIME_UNITS_MS = {
+  years: 0.5 * YEAR_MS,
+  months: 0.5 * MONTH_MS,
+  weeks: 0.5 * WEEK_MS,
+  days: 0.5 * DAY_MS,
+  hours: 0.5 * HOUR_MS,
+  minutes: 0.5 * MINUTE_MS,
+  seconds: 0.5 * SECOND_MS,
+  milliseconds: 0.5 * MILLISECOND_MS,
 } as const
