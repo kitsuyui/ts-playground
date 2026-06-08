713c6a4a3ae2f065beb2d5eb3087fa4b375c05a1
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-08 09:19:55 +0900

    fix: replace global Array augmentation with exported NumberArray type

--- packages/symbolic-prototype/src/number-array/index.spec.ts
+++ packages/symbolic-prototype/src/number-array/index.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest'
 
-import { Symbols } from './index'
+import { type NumberArray, Symbols } from './index'
 
 describe('NumberArray', () => {
   it('scale', () => {
-    const array: number[] = [1, 2, 3]
+    const array = [1, 2, 3] as unknown as NumberArray
     const result = array[Symbols.scale](2)
     expect(result).toEqual([2, 4, 6])
   })
